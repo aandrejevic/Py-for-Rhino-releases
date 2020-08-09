a6ac62387aa26f8104459ac341db11edd913af79
aandrejevic <38728047+aandrejevic@users.noreply.github.com>
2020-08-09 10:20:46 +0200

    Create Arrows_oncurve.py

---
+++ src/Arrows_oncurve.py
@@ -0,0 +1,38 @@
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+
+def arrowCurve():
+
+    curves = rs.GetObjects("select curves to change into with arrows",4, preselect=True)
+    if not curves:
+        return
+    s=sc.sticky['scale'] if sc.sticky.has_key('scale') else 20
+    scale = rs.GetReal("scale of the arrow curve", s, 5, 100)
+
+
+    if not scale:
+        return
+    sc.sticky['scale']=scale
+
+    rs.EnableRedraw(False)
+
+    for curve in curves:
+        lines=[]
+        if rs.CurveLength(curve)>scale:
+            pts = rs.DivideCurveLength(curve, scale)
+            for pt in pts:
+                t=rs.CurveClosestPoint(curve, pt)
+                vec = rs.CurveTangent(curve, t)*scale/10
+                line = rs.AddLine(pt, pt+vec)
+                line_copy = rs.CopyObject(line, [0,0,0])
+                rs.RotateObject(line, pt, 45)
+                rs.RotateObject(line_copy, pt, -45)
+                lines.append(line)
+                lines.append(line_copy)
+            group = rs.AddGroup()
+            rs.AddObjectsToGroup(lines, group)
+            rs.AddObjectsToGroup(curve, group)
+            rs.SelectObjects(lines)
+            rs.SelectObjects(curves)
+    rs.EnableRedraw(True)
+arrowCurve()
